062119d6a737c684d2fb21ebe049f5e1e91e7361
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-10-19 19:01:26 -0300

    start dockerizing project

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,11 @@
+import { IsString } from 'class-validator';
 export class CreateReservationDto {
+  @IsString()
   start_date: string;
+  @IsString()
   end_date: string;
+  @IsString()
   place_id: string;
+  @IsString()
   invoice_id: string;
 }
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, LoggerModule } from '@app/common';
 import { PrismaService } from '@app/common/database/PrismaServices';
 import { ReservationRepository } from './reservations.repository';
 
+import { pinoHttp } from 'pino-http';
+
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, LoggerModule],
   controllers: [ReservationsController],
   providers: [ReservationsService, PrismaService, ReservationRepository],
 })
